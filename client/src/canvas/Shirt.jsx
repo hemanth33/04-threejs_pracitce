3bf09a3525744e65e7b08648b248cdf8cb6bb429
hemanth33 <hemanthnayak2001@gmail.com>
2023-11-28 10:48:01 +0530

    Initial commit

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Model from '../../public/Shirt_baked.jsx'
+
+const Shirt = () => {
+
+  return (
+    <group>
+      <Model />
+    </group>
+  );
+};
+
+export default Shirt;
---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import config from '../config/config'
+import state from '../store'
+import { download } from '../assets'
+import { downloadCanvasToImage, reader } from '../config/helpers'
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'
+import { fadeAnimation, slideAnimation } from '../config/motion'
+import { AIPicker, ColorPicker, FilePicker, Tab, CustomButton } from '../components'
+
+const Customizer = () => {
+
+  const snap = useSnapshot(state);
+
+  const[file, setFile] = useState('');
+  const[prompt, setPrompt] = useState('');
+  const[generateImg, setGenerateImg] = useState(false);
+  const[activeEditorTab, setActiveEditorTab] = useState('');
+  const[activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  })
+
+  const generateTabContent = () => {
+      switch (activeEditorTab) {
+        case "colorpicker":
+          return <ColorPicker />
+        case "filepicker":
+          return <FilePicker
+            file={file}
+            setFile={setFile}
+            readFile={readFile}
+          />
+        case "aipicker":
+          return <AIPicker />
+        default:
+          return null;
+      }
+  }
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+    state[decalType.stateProperty] = result;
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  }
+
+  const handleActiveFilterTab = (tabname) => {
+    switch (tabname) {
+      case "logoShirt":
+          state.isLogoTexture = !activeFilterTab[tabname];  
+        break;
+      case "stylishShirt":
+          state.isFullTexture = !activeFilterTab[tabname];  
+        break;
+      default:
+          state.isLogoTexture = true;
+          state.isFullTexture = false;
+    }
+  }
+
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type, result);
+        setActiveEditorTab("");
+      })
+  }
+
+  return (
+    <AnimatePresence >
+      {!snap.intro && (
+        <>
+          <motion.div 
+            key="custom"
+            className='absolute top-0 left-0 z-10'
+            {...slideAnimation('left')}
+            >
+
+              <div className='flex items-center min-h-screen'>
+                <div className='editortabs-container tabs'>
+                  {EditorTabs.map((tab) => (
+                    <Tab
+                      key={tab.name}
+                      tab={tab}
+                      handleClick={() => setActiveEditorTab(tab.name)}
+                    />
+                  ))}
+
+                  {generateTabContent()}
+                </div>
+              </div>
+
+          </motion.div>
+
+          <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => state.intro = true}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+
+          <motion.div className='filtertabs-container' {...slideAnimation('up')}>
+
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab = ""
+                handleClick={() => {}}
+              />
+            ))}
+
+          </motion.div>
+        </>
+      )}
+
+    </AnimatePresence>
+  )
+}
+
+export default Customizer
